c491375408620a1bbf55093149bd3ce1d4685235
Ivan Grynenko <ivan@redyhost.com>
2015-01-30 21:28:18 +1100

    Added some libraries, modules and minor CSS fixes

--- sites/all/libraries/ckeditor/config.js
+++ sites/all/libraries/ckeditor/config.js
@@ -7,4 +7,24 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+  config.extraPlugins = 'pbckcode';
+  // ADVANCED CONTENT FILTER (ACF)
+  //     // ACF protects your CKEditor instance of adding unofficial tags
+  //         // however it strips out the pre tag of pbckcode plugin
+  //             // add this rule to enable it, useful when you want to re edit a post
+  //                 // Only needed on v1.1.x and v1.2.0
+  config.allowedContent= 'pre[*]{*}(*)';
+  config.pbckcode = {
+    cls : '',
+
+    highlighter : 'PRETTIFY',
+
+    modes : [ ['HTML', 'html'], ['CSS', 'css'], ['PHP', 'php'], ['JS', 'javascript'] ],
+
+    theme : 'textmate',
+
+    tab_size : '2',
+
+    js : "http://cdn.jsdelivr.net//ace/1.1.4/noconflict/"
+  };
 };
