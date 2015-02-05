aeda71ca2523b09e42478bb5eff2abe5f113e1a8
Ivan Grynenko <ivan@redyhost.com>
2015-02-06 09:11:00 +1100

    Moved cc to cc-disabled

---
+++ cc-disabled/configuration.php
@@ -0,0 +1,14 @@
+<?php
+$license = 'Owned-81994cf22bf9e9f94983';
+$db_host = '127.0.0.1';
+$db_username = 'rh_drupal_mql';
+$db_password = 'i+!8-vnx<WnK#iHx#z3k|SVjd).}p{e&wc@YPg^';
+$db_name = 'rh_hmcs';
+$cc_encryption_hash = '4gsl2eT3wywtY9vYb0GerQFFNRdC8Uf30xfSCYMZn8ic8E09uwdLdJhQrljp5ojW';
+$templates_compiledir = '/var/www/html/redyhost/templates_c/';
+$attachments_dir = '/var/www/html/redyhost/attachments/';
+$downloads_dir = '/var/www/html/redyhost/downloads/';
+$customadminpath = 'whmcsadmin2';
+$api_access_key = 'Owned-81994cf22bf9e9f94983';
+$display_errors = FALSE;
+?>
