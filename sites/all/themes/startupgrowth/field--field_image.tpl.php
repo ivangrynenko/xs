f9569af2a42bb1430fcac62dff25e498b6ca3192
Ivan Grynenko <ivan.grynenko@eurekareport.com.au>
2015-02-13 08:00:10 +1100

    Removed overlay and link on image_field.

--- sites/all/themes/startupgrowth/field--field_image.tpl.php
+++ sites/all/themes/startupgrowth/field--field_image.tpl.php
@@ -13,51 +13,51 @@ endif;
 
 <?php $node = $element['#object']; ?>
 
-  <div class="images-container clearfix">
+<div class="images-container clearfix">
 
-    <div class="image-preview">
+  <div class="image-preview">
 
-      <a class="image-popup overlayed" href="<?php print file_create_url($node->field_image[LANGUAGE_NONE][0]['uri']); ?>" title="<?php print $node->field_image[LANGUAGE_NONE][0]['title']; ?>">
-        <img src="<?php print image_style_url('large', $node->field_image[LANGUAGE_NONE][0]['uri']); ?>" alt="<?php print $node->field_image[LANGUAGE_NONE][0]['alt']; ?>" title="<?php print $node->field_image[LANGUAGE_NONE][0]['title']; ?>"/>
-        <span class="overlay large"><i class="fa fa-plus"></i></span>
-      </a>
+    <!--      <a class="image-popup overlayed" href="--><?php //print file_create_url($node->field_image[LANGUAGE_NONE][0]['uri']); ?><!--" title="--><?php //print $node->field_image[LANGUAGE_NONE][0]['title']; ?><!--">-->
+    <img src="<?php print image_style_url('large', $node->field_image[LANGUAGE_NONE][0]['uri']); ?>" alt="<?php print $node->field_image[LANGUAGE_NONE][0]['alt']; ?>" title="<?php print $node->field_image[LANGUAGE_NONE][0]['title']; ?>"/>
+    <!--        <span class="overlay large"><i class="fa fa-plus"></i></span>-->
+    <!--      </a>-->
 
-      <?php if ($node->field_image[LANGUAGE_NONE][0]['title'] || $node->field_image[LANGUAGE_NONE][0]['alt']) : ?>
-        <div class="image-caption hidden-xs">
-          <?php if ($node->field_image[LANGUAGE_NONE][0]['title']) : ?>
-            <h4><?php print $node->field_image[LANGUAGE_NONE][0]['title']; ?></h4>
-          <?php endif; ?>
-          <?php if ($node->field_image[LANGUAGE_NONE][0]['alt']) : ?>
-            <p><?php print $node->field_image[LANGUAGE_NONE][0]['alt']; ?></p>
-          <?php endif; ?>
-        </div>
-      <?php endif; ?>
-
-    </div>
-
-    <?php $numberOfImages = 0;
-    foreach ($node->field_image[LANGUAGE_NONE] as $key => $file) {
-      $numberOfImages++;
-    } ?>
-
-    <?php if ($numberOfImages > 1) { ?>
-
-      <div class="image-listing-items">
-        <?php $i = 0;
-        foreach ($node->field_image[LANGUAGE_NONE] as $key => $file) {
-          if ($key == 0) {
-            continue;
-          }
-          $i++; ?>
-          <div class="image-listing-item">
-            <a class="image-popup overlayed" href="<?php print file_create_url($node->field_image[LANGUAGE_NONE][$key]['uri']) ?>" title="<?php print $node->field_image[LANGUAGE_NONE][$key]['title']; ?>">
-              <img src="<?php print image_style_url('small', $node->field_image[LANGUAGE_NONE][$key]['uri']); ?>" alt="<?php print $node->field_image[LANGUAGE_NONE][$key]['alt']; ?>" title="<?php print $node->field_image[LANGUAGE_NONE][$key]['title']; ?>"/>
-              <span class="overlay small"><i class="fa fa-plus"></i></span>
-            </a>
-          </div>
-        <?php } ?>
-
+    <?php if ($node->field_image[LANGUAGE_NONE][0]['title'] || $node->field_image[LANGUAGE_NONE][0]['alt']) : ?>
+      <div class="image-caption hidden-xs">
+        <?php if ($node->field_image[LANGUAGE_NONE][0]['title']) : ?>
+          <h4><?php print $node->field_image[LANGUAGE_NONE][0]['title']; ?></h4>
+        <?php endif; ?>
+        <?php if ($node->field_image[LANGUAGE_NONE][0]['alt']) : ?>
+          <p><?php print $node->field_image[LANGUAGE_NONE][0]['alt']; ?></p>
+        <?php endif; ?>
       </div>
-    <?php } ?>
+    <?php endif; ?>
 
   </div>
+
+  <?php $numberOfImages = 0;
+  foreach ($node->field_image[LANGUAGE_NONE] as $key => $file) {
+    $numberOfImages++;
+  } ?>
+
+  <?php if ($numberOfImages > 1) { ?>
+
+    <div class="image-listing-items">
+      <?php $i = 0;
+      foreach ($node->field_image[LANGUAGE_NONE] as $key => $file) {
+        if ($key == 0) {
+          continue;
+        }
+        $i++; ?>
+        <div class="image-listing-item">
+          <a class="image-popup overlayed" href="<?php print file_create_url($node->field_image[LANGUAGE_NONE][$key]['uri']) ?>" title="<?php print $node->field_image[LANGUAGE_NONE][$key]['title']; ?>">
+            <img src="<?php print image_style_url('small', $node->field_image[LANGUAGE_NONE][$key]['uri']); ?>" alt="<?php print $node->field_image[LANGUAGE_NONE][$key]['alt']; ?>" title="<?php print $node->field_image[LANGUAGE_NONE][$key]['title']; ?>"/>
+            <span class="overlay small"><i class="fa fa-plus"></i></span>
+          </a>
+        </div>
+      <?php } ?>
+
+    </div>
+  <?php } ?>
+
+</div>
