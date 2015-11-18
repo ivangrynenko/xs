0686da9122b4830b24c44f4e6463204f25592f80
Ivan Grynenko <ivan.grynenko@eurekareport.com.au>
2015-11-19 08:01:08 +1100

    Updated display to avoid API errors and empty fields in the case when connection to xenserver pool is lost.

--- templates/xs-actions-snapshots-output.tpl.php
+++ templates/xs-actions-snapshots-output.tpl.php
@@ -21,29 +21,44 @@
 ?>
 <section class="node node-snapshots">
   <div class="row">
-    <?php if (empty($snapshots_max)) : ?>
-      <div class="col-md-12">
-        <h3>Snapshots disabled</h3>
 
-        <p>
-          No snapshots could be created for this server.
-        </p>
+    <?php if ($error_message) : ?>
+      <div class="col-md-12">
+        <?php print $error_message; ?>
       </div>
+
+
     <?php else: ?>
-      <div class="col-md-12">
-        <h3>Available snapshots</h3>
 
-        <p>You have available <?php print $snapshots_qty; ?> out of the maximum <?php print $snapshots_max; ?></p>
+      <?php if (empty($snapshots_max)) : ?>
+        <div class="col-md-12">
+          <h3>Snapshots disabled</h3>
 
-        <p><?php print $link; ?></p>
+          <p>
+            No snapshots could be created for this server.
+          </p>
+        </div>
+      <?php else: ?>
+        <div class="col-md-12">
+          <h3>Available snapshots</h3>
 
-      </div>
-    <div class="col-md-8">
-      <?php print $snapshots_table; ?>
-      </div>
-    <div class="col-md-4">
-      <?php print $snapshots_actions_form; ?>
-    </div>
+          <p>You have available <?php print $snapshots_qty; ?> out of the maximum <?php print $snapshots_max; ?></p>
+
+          <p><?php print $link; ?></p>
+
+        </div>
+
+        <div class="col-md-12">
+          <div class="help">
+            <p>To destroy a snapshot, select 'Trash' in the Actions column. To revert your Server to the selected snapshot, select 'Revert'</p>
+            <p>A confirmation screen will appear next, to confirm your action.</p>
+          </div>
+        </div>
+
+        <div class="col-md-12">
+          <?php print $snapshots_table; ?>
+        </div>
+      <?php endif; ?>
     <?php endif; ?>
   </div>
 </section>
